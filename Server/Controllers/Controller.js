8604b708410338163cc0d98c513b636b0f2dbed3
Daniel <danielajagbe1234@gmail.com>
2024-10-01 17:55:21 +0100

    Result Controller

--- Server/Controllers/Controller.js
+++ Server/Controllers/Controller.js
@@ -1,5 +1,34 @@
 
 
+/** Get all questions */
+
 export async function GetQuestions(req, res) {
   res.json("Questions api get request")
+}
+
+/** Insert all questions */
+
+export async function InsertQuestions(req, res) {
+  res.json("Questions api post request")
+}
+
+/** Delete all questions */
+
+export async function DropQuestions(req, res) {
+  res.json("Questions api delete request")
+}
+
+/** Get all result */
+export async function GetResult(req, res) {
+  res.json("Result api get request")
+}
+
+/** Post all results */
+export async function StoreResult(req, res) {
+  res.json("Result api post request")
+}
+
+/** Delete all results */
+export async function DropResult(req, res) {
+  res.json("Result api delete request")
 }
--- Server/Router/Route.js
+++ Server/Router/Route.js
@@ -8,9 +8,17 @@ import * as Controller from '../Controllers/Controller.js'
 
 /** Questions Routes API*/
 
-router.get("/questions", Controller.GetQuestions)
+// router.get("/questions", Controller.GetQuestions)
+// router.post("/questions", Controller.InsertQuestions)
 
+/** Because we have the same endpoint ("/questions") */
+router.route("/questions")
+    .get(Controller.GetQuestions) /** GET Request */
+    .post(Controller.InsertQuestions) /** POST Request */
+    .delete(Controller.DropQuestions) /** DELETE Request */
 
+router.route("/result")
+    .get(Controller.GetResult)
 
 
 export default router;
